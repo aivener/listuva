52393d786e2383870b09506e80577a492e786f7c
Niti Paudyal <np8bx@virginia.edu>
2016-02-18 17:03:36 -0500

    deleting extraneous folder

--- uvalist-models-2/uvalist/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Student, Category, Subcategory, Post, Comment
-
-admin.site.register(Student)
-admin.site.register(Category)
-admin.site.register(Subcategory)
-admin.site.register(Post)
-admin.site.register(Comment)
