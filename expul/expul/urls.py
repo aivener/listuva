0632496f11363ab98875e5c221b42a01a0c171be
Allie Ivener <adi2hf@virginia.edu>
2016-02-21 16:26:32 -0500

    working on exp layer

--- expul/expul/urls.py
+++ expul/expul/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from . import views
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/v1/maketable$', views.maketable, name="maketable"),
+
 ]
--- expul/expul/views.py
+++ expul/expul/views.py
@@ -9,7 +9,12 @@ from django.views.decorators.csrf import csrf_exempt
 
 #get individual item info (if click it) -- price, comments, etc. - build it together, serialize into JSON, pass to top layer to turn into something to look at
 
-def getHomePageInfo:
+def makeTable():
+	#get json of all categories
+	result= requests.get('modelsul/api/v1/category')
+	# status_code = result.status_code
+	return HttpResponse(result, content_type='json')
+
 	#get list of all posts
 		#python request to models/api/v1/posts which returns json of all the posts
 	#take that json, and get json of other stuff needed on home page
