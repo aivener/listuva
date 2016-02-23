c0461ae785226b1f5a56f09c7566754d3f257ff4
Allie Ivener <adi2hf@virginia.edu>
2016-02-23 17:13:58 -0500

    exp layer json for categories, fixed command in .yml

--- expul/expul/views.py
+++ expul/expul/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 from django.core import serializers
 from django.shortcuts import get_object_or_404
-from django.views.decorators.csrf import csrf_exempt 
+import json
 import requests
 
 #get all info for home page
@@ -11,9 +11,21 @@ import requests
 
 def makeTable(request):
 	#get json of all categories
-	result = requests.get('http://modelsul:8000/api/v1/category')
-	# status_code = result.status_code
-	return HttpResponse(result, content_type='json')
+	all_cats = requests.get('http://modelsul:8000/api/v1/category')
+	all_subcats = requests.get('http://modelsul:8000/api/v1/subcategory')
+	new_info = {}
+	deser_cats = json.loads(all_cats.text)
+	deser_subcats = json.loads(all_subcats.text)
+	for cat in deser_cats:	
+		curr_cat_name = str(cat['fields']['title'])
+		curr_cat_id = cat['pk']
+		new_info[curr_cat_name] = []
+		for subcat in deser_subcats:
+			if(subcat['fields']['category'] == curr_cat_id):
+				new_info[curr_cat_name].append(subcat['fields']['title'])
+	ser = json.dumps(new_info)
+	return HttpResponse(ser, content_type='application/json')
+	
 
 	#get list of all posts
 		#python request to models/api/v1/posts which returns json of all the posts
