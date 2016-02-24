c03bf886f49bf7eb585385c7561682dc61e0a3f1
Allie Ivener <adi2hf@virginia.edu>
2016-02-24 16:56:29 -0500

    reformatted json for web layer home page

--- frontul/frontul/views.py
+++ frontul/frontul/views.py
@@ -10,20 +10,13 @@ def displayCells(request):
 	cats_subcats = requests.get('http://expul:8000/api/v1/maketable')
 	deser = json.loads(cats_subcats.text)
 	cells_dict = {}
-	for cat in deser:
-		cells_dict[cat] = []
-		for subcat in deser[cat]:
-			cells_dict[cat].append(subcat)
+	for curr_cat in deser: #curr_cat is the name of the category
+		cells_dict[curr_cat] = {}
+		print(curr_cat)
+		cells_dict[curr_cat]["id"] = deser[curr_cat]['catID']
+		cells_dict[curr_cat]["subcategories"] = deser[curr_cat]['subcategories']
+		# for subcat in deser[cat]:
+		# 	# cells_dict[cat].append(subcat)
+		# 	cells_dict[cat][subcat] = subcat
 	return render(request, 'home.html', {'cells_dict':cells_dict})
 	#return JsonResponse(cells_dict, content_type='json', safe=False)
-
-# def linkToPosts(request):
-# 	cats_subcats = requests.get('http://expul:8000/api/v1/maketable')
-# 	deser = json.loads(cats_subcats.text)
-# 	cells_dict = {}
-# 	for cat in deser:
-# 		cells_dict[cat] = []
-# 		for subcat in deser[cat]:
-# 			cells_dict[cat].append(subcat)
-# 	return render(request, 'home.html', {'cells_dict':cells_dict})
-# 	#return JsonResponse(cells_dict, content_type='json', safe=False)
